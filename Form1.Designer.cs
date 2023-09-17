d75fa7084b1151bffe418004503e91c6ee1c1edb
josefabiodev <josefabiodev@gmail.com>
2023-09-17 06:51:37 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace TelaDeLogin
+{
+    partial class Frm_Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pnl_Logo = new System.Windows.Forms.Panel();
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.Txt_Username = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.Lbl_Password = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.Txt_Password = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.bunifuFlatButton1 = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.Btn_Cancel = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bunifuCustomLabel2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Pnl_Logo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Pnl_Logo
+            // 
+            this.Pnl_Logo.BackColor = System.Drawing.Color.SteelBlue;
+            this.Pnl_Logo.Controls.Add(this.pictureBox2);
+            this.Pnl_Logo.Controls.Add(this.pictureBox1);
+            this.Pnl_Logo.Controls.Add(this.bunifuCustomLabel2);
+            this.Pnl_Logo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Pnl_Logo.Location = new System.Drawing.Point(0, 0);
+            this.Pnl_Logo.Name = "Pnl_Logo";
+            this.Pnl_Logo.Size = new System.Drawing.Size(248, 389);
+            this.Pnl_Logo.TabIndex = 0;
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(341, 43);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(71, 17);
+            this.bunifuCustomLabel1.TabIndex = 1;
+            this.bunifuCustomLabel1.Text = "Username";
+            // 
+            // Txt_Username
+            // 
+            this.Txt_Username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Txt_Username.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.Txt_Username.ForeColor = System.Drawing.Color.Silver;
+            this.Txt_Username.HintForeColor = System.Drawing.Color.Silver;
+            this.Txt_Username.HintText = "";
+            this.Txt_Username.isPassword = false;
+            this.Txt_Username.LineFocusedColor = System.Drawing.Color.SteelBlue;
+            this.Txt_Username.LineIdleColor = System.Drawing.Color.Gray;
+            this.Txt_Username.LineMouseHoverColor = System.Drawing.Color.SkyBlue;
+            this.Txt_Username.LineThickness = 3;
+            this.Txt_Username.Location = new System.Drawing.Point(344, 52);
+            this.Txt_Username.Margin = new System.Windows.Forms.Padding(4);
+            this.Txt_Username.Name = "Txt_Username";
+            this.Txt_Username.Size = new System.Drawing.Size(262, 44);
+            this.Txt_Username.TabIndex = 2;
+            this.Txt_Username.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // Lbl_Password
+            // 
+            this.Lbl_Password.AutoSize = true;
+            this.Lbl_Password.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Password.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Lbl_Password.Location = new System.Drawing.Point(341, 125);
+            this.Lbl_Password.Name = "Lbl_Password";
+            this.Lbl_Password.Size = new System.Drawing.Size(69, 17);
+            this.Lbl_Password.TabIndex = 3;
+            this.Lbl_Password.Text = "Password";
+            // 
+            // Txt_Password
+            // 
+            this.Txt_Password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Txt_Password.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.Txt_Password.ForeColor = System.Drawing.Color.Silver;
+            this.Txt_Password.HintForeColor = System.Drawing.Color.Silver;
+            this.Txt_Password.HintText = "";
+            this.Txt_Password.isPassword = false;
+            this.Txt_Password.LineFocusedColor = System.Drawing.Color.SteelBlue;
+            this.Txt_Password.LineIdleColor = System.Drawing.Color.Gray;
+            this.Txt_Password.LineMouseHoverColor = System.Drawing.Color.SkyBlue;
+            this.Txt_Password.LineThickness = 3;
+            this.Txt_Password.Location = new System.Drawing.Point(344, 134);
+            this.Txt_Password.Margin = new System.Windows.Forms.Padding(4);
+            this.Txt_Password.Name = "Txt_Password";
+            this.Txt_Password.Size = new System.Drawing.Size(262, 44);
+            this.Txt_Password.TabIndex = 4;
+            this.Txt_Password.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // bunifuFlatButton1
+            // 
+            this.bunifuFlatButton1.Activecolor = System.Drawing.Color.SteelBlue;
+            this.bunifuFlatButton1.BackColor = System.Drawing.Color.SteelBlue;
+            this.bunifuFlatButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuFlatButton1.BorderRadius = 0;
+            this.bunifuFlatButton1.ButtonText = "Login";
+            this.bunifuFlatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuFlatButton1.DisabledColor = System.Drawing.Color.Gray;
+            this.bunifuFlatButton1.Iconcolor = System.Drawing.Color.Transparent;
+            this.bunifuFlatButton1.Iconimage = null;
+            this.bunifuFlatButton1.Iconimage_right = null;
+            this.bunifuFlatButton1.Iconimage_right_Selected = null;
+            this.bunifuFlatButton1.Iconimage_Selected = null;
+            this.bunifuFlatButton1.IconMarginLeft = 0;
+            this.bunifuFlatButton1.IconMarginRight = 0;
+            this.bunifuFlatButton1.IconRightVisible = true;
+            this.bunifuFlatButton1.IconRightZoom = 0D;
+            this.bunifuFlatButton1.IconVisible = true;
+            this.bunifuFlatButton1.IconZoom = 90D;
+            this.bunifuFlatButton1.IsTab = false;
+            this.bunifuFlatButton1.Location = new System.Drawing.Point(344, 212);
+            this.bunifuFlatButton1.Name = "bunifuFlatButton1";
+            this.bunifuFlatButton1.Normalcolor = System.Drawing.Color.SteelBlue;
+            this.bunifuFlatButton1.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.bunifuFlatButton1.OnHoverTextColor = System.Drawing.Color.White;
+            this.bunifuFlatButton1.selected = false;
+            this.bunifuFlatButton1.Size = new System.Drawing.Size(262, 48);
+            this.bunifuFlatButton1.TabIndex = 5;
+            this.bunifuFlatButton1.Text = "Login";
+            this.bunifuFlatButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bunifuFlatButton1.Textcolor = System.Drawing.Color.White;
+            this.bunifuFlatButton1.TextFont = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // Btn_Cancel
+            // 
+            this.Btn_Cancel.Activecolor = System.Drawing.Color.SteelBlue;
+            this.Btn_Cancel.BackColor = System.Drawing.Color.SteelBlue;
+            this.Btn_Cancel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Btn_Cancel.BorderRadius = 0;
+            this.Btn_Cancel.ButtonText = "Cancel";
+            this.Btn_Cancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_Cancel.DisabledColor = System.Drawing.Color.Gray;
+            this.Btn_Cancel.Iconcolor = System.Drawing.Color.Transparent;
+            this.Btn_Cancel.Iconimage = null;
+            this.Btn_Cancel.Iconimage_right = null;
+            this.Btn_Cancel.Iconimage_right_Selected = null;
+            this.Btn_Cancel.Iconimage_Selected = null;
+            this.Btn_Cancel.IconMarginLeft = 0;
+            this.Btn_Cancel.IconMarginRight = 0;
+            this.Btn_Cancel.IconRightVisible = true;
+            this.Btn_Cancel.IconRightZoom = 0D;
+            this.Btn_Cancel.IconVisible = true;
+            this.Btn_Cancel.IconZoom = 90D;
+            this.Btn_Cancel.IsTab = false;
+            this.Btn_Cancel.Location = new System.Drawing.Point(344, 284);
+            this.Btn_Cancel.Name = "Btn_Cancel";
+            this.Btn_Cancel.Normalcolor = System.Drawing.Color.SteelBlue;
+            this.Btn_Cancel.OnHovercolor = System.Drawing.Color.Red;
+            this.Btn_Cancel.OnHoverTextColor = System.Drawing.Color.White;
+            this.Btn_Cancel.selected = false;
+            this.Btn_Cancel.Size = new System.Drawing.Size(262, 48);
+            this.Btn_Cancel.TabIndex = 6;
+            this.Btn_Cancel.Text = "Cancel";
+            this.Btn_Cancel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Btn_Cancel.Textcolor = System.Drawing.Color.White;
+            this.Btn_Cancel.TextFont = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(338, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 25);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Forgot your password?";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.AutoSize = true;
+            this.bunifuCustomLabel2.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel2.ForeColor = System.Drawing.Color.White;
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(52, 126);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(147, 26);
+            this.bunifuCustomLabel2.TabIndex = 8;
+            this.bunifuCustomLabel2.Text = "Login screen";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TelaDeLogin.Properties.Resources.logo_removebg_preview;
+            this.pictureBox2.Location = new System.Drawing.Point(47, -10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(152, 96);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TelaDeLogin.Properties.Resources.Itzikgur_My_Seven_Keys_256;
+            this.pictureBox1.Location = new System.Drawing.Point(59, 164);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(133, 136);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(696, 389);
+            this.Controls.Add(this.Btn_Cancel);
+            this.Controls.Add(this.bunifuFlatButton1);
+            this.Controls.Add(this.Lbl_Password);
+            this.Controls.Add(this.Txt_Password);
+            this.Controls.Add(this.bunifuCustomLabel1);
+            this.Controls.Add(this.Pnl_Logo);
+            this.Controls.Add(this.Txt_Username);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Frm_Login";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tela de Login";
+            this.Pnl_Logo.ResumeLayout(false);
+            this.Pnl_Logo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Pnl_Logo;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox Txt_Username;
+        private Bunifu.Framework.UI.BunifuCustomLabel Lbl_Password;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox Txt_Password;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton1;
+        private Bunifu.Framework.UI.BunifuFlatButton Btn_Cancel;
+        private System.Windows.Forms.Button button1;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
